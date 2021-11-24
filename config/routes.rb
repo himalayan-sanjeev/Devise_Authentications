418e266fe77b27decb5c717603457ab922d40a59
Sanjeev Poudel <sanjeevpoudel111@gmail.com>
2021-11-24 15:25:09 +0545

    devise trackable module added

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
+  resources :users, only: [:index] 
+  
   get 'home',to:'home#homepage'
   get 'dashboard', to:'home#dashboard'
-  
+
   root to: "home#homepage"
 
 
